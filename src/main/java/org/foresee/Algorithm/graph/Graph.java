3b2fdff20bd49710287d6d7d0940b24400a1cf98
Foresee2016 <1123520670@qq.com>
2018-05-31 14:17:47 +0800

    Extract main() to DemoSimple for a simple search use.

--- src/main/java/org/foresee/Algorithm/graph/Graph.java
+++ src/main/java/org/foresee/Algorithm/graph/Graph.java
@@ -10,21 +10,6 @@ import java.util.Queue;
  * 邻接矩阵适合表示稠密图（边E的数目接近V的平方），另外，如果需要快速判断任意两个结点是否有边相连，适合邻接矩阵
  */
 public class Graph {
-	public static void main(String[] args) {
-		Vertex v1=new Vertex(1);
-		Vertex v2=new Vertex(2);
-		Vertex v3=new Vertex(3);
-		Vertex v4=new Vertex(4);
-		Vertex v5=new Vertex(5);
-		Graph graph=new Graph();
-		graph.addVertex(v1, v2,v5);
-		graph.addVertex(v2, v1,v5,v3,v4);
-		graph.addVertex(v3, v2,v4);
-		graph.addVertex(v4, v2,v5,v3);
-		graph.addVertex(v5, v4,v1,v2);
-		graph.broadFirstSearch(v1);
-		graph.printPath(v1, v4);
-	}
 	public static final int INFINITY=1000;
 	public ArrayList<LinkedList<Vertex>> adjoin;
 	public Graph() {
