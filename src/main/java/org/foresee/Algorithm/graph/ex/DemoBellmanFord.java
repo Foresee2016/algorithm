850baf98ca6110f7a49e4ecd82dfdcdfc0d93d4a
Foresee2016 <1123520670@qq.com>
2018-06-14 10:41:11 +0800

    Some Comment. And a minus circle test case in DemoBellmanFord.

--- src/main/java/org/foresee/Algorithm/graph/ex/DemoBellmanFord.java
+++ src/main/java/org/foresee/Algorithm/graph/ex/DemoBellmanFord.java
@@ -22,9 +22,45 @@ public class DemoBellmanFord {
 		graph.addVertexAndWeight(z, Arrays.asList(x, s), Arrays.asList(7.0, 2.0));
 
 		SingleSourceShortestPath shortestPath = new SingleSourceShortestPath();
-		shortestPath.BellmanFord(graph, s);
-		for (Vertex v : graph.vertexs) {
-			System.out.println(v.name + ", d = " + v.d);
+		if (shortestPath.BellmanFord(graph, s)) {
+			for (Vertex v : graph.vertexs) {
+				System.out.println(v.name + ", d = " + v.d);
+			}
+		} else {
+			System.out.println("包含负权重值的环路，不能求得最短路径。");
+		}
+		System.out.println("---------我是分割线---------");
+		testMinusCircle();
+	}
+	/**
+	 * 验证BellmanFord算法能识别负权重值的环路
+	 */
+	public static void testMinusCircle() {
+		Vertex s=new Vertex("s");
+		Vertex a=new Vertex("a");
+		Vertex b=new Vertex("b");
+		Vertex c=new Vertex("c");
+		Vertex d=new Vertex("d");
+		Vertex e=new Vertex("e");
+		Vertex f=new Vertex("f");
+		Vertex g=new Vertex("g");
+		ShortestPathGraph graph=new ShortestPathGraph();
+		graph.addVertexAndWeight(s, Arrays.asList(a,c,e), Arrays.asList(3.0, 5.0, 2.0));
+		graph.addVertexAndWeight(a, Arrays.asList(b), Arrays.asList(-4.0));
+		graph.addVertexAndWeight(b, Arrays.asList(g), Arrays.asList(4.0));
+		graph.addVertexAndWeight(c, Arrays.asList(d), Arrays.asList(6.0));
+		graph.addVertexAndWeight(d, Arrays.asList(c, g), Arrays.asList(-3.0, 8.0));
+		graph.addVertexAndWeight(e, Arrays.asList(f), Arrays.asList(3.0));
+		graph.addVertexAndWeight(f, Arrays.asList(e, g), Arrays.asList(-6.0, 7.0));
+		graph.addVertex(g);
+		
+		SingleSourceShortestPath shortestPath = new SingleSourceShortestPath();
+		if (shortestPath.BellmanFord(graph, s)) {
+			for (Vertex v : graph.vertexs) {
+				System.out.println(v.name + ", d = " + v.d);
+			}
+		} else {
+			System.out.println("testMinusCircle()：包含负权重值的环路，不能求得最短路径。");
 		}
 	}
 }
