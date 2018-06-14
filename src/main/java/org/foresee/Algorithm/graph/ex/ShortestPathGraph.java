06df18f43916b76f422fd42f38cf7f4e4eb78e6f
Foresee2016 <1123520670@qq.com>
2018-06-14 11:43:33 +0800

    Modify ShortestPathGraph for dagShortestPath() use.

--- src/main/java/org/foresee/Algorithm/graph/ex/ShortestPathGraph.java
+++ src/main/java/org/foresee/Algorithm/graph/ex/ShortestPathGraph.java
@@ -37,12 +37,18 @@ public class ShortestPathGraph {
 	public static class Vertex {
 		public String name; // 结点名称
 		public LinkedList<Edge> adjacents; // 邻接结点
-		public double d; // NOTE：这里表示权重和，而不是距离了
+		// NOTE：单源最短路径算法中表示权重和，而不是距离了。拓扑排序中表示发现时间。
+		public double d; 
+		public int f; // 深度优先遍历时存涂黑色时间戳
 		public Vertex parent; // 图搜索时指向前驱结点
+		public Color color; // 颜色，图搜索时标记
 		public Vertex(String name) {
 			this.name = name;
 			adjacents = new LinkedList<>();
 		}
+	}
+	public enum Color {
+		white, gray, black
 	}
 	public static class Edge {
 		public Vertex src; // 后边需要排序和遍历边，还是加上源节点
