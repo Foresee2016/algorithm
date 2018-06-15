e50d3bae047305cedbaf877b8d1e6f5110a73f9b
Foresee2016 <1123520670@qq.com>
2018-06-15 17:01:38 +0800

    GlobalShortestPath, a new file with some comment.

---
+++ src/main/java/org/foresee/Algorithm/graph/ex/GlobalShortestPath.java
@@ -0,0 +1,13 @@
+package org.foresee.Algorithm.graph.ex;
+
+/**
+ * 所有结点之间的最短路径问题，如计算所有城市之间交通道路距离时。
+ */
+public class GlobalShortestPath {
+	/**
+	 * 在给定W和L(m-1)情况下，计算L(m)，将最近计算出的最短路径扩展了一条边
+	 */
+	public void extendShortestPaths(MatrixGraph graph) {
+		
+	}
+}
