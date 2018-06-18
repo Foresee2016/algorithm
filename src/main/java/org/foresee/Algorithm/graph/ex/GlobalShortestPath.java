963b609e77bd76ab19b4d39da0c129002d95a648
Foresee2016 <1123520670@qq.com>
2018-06-18 16:05:29 +0800

    Use shortest path weight matrix L generate Parent matrix pi.

--- src/main/java/org/foresee/Algorithm/graph/ex/GlobalShortestPath.java
+++ src/main/java/org/foresee/Algorithm/graph/ex/GlobalShortestPath.java
@@ -69,4 +69,51 @@ public class GlobalShortestPath {
 		}
 		return C;
 	}
+	/**
+	 * 练习25.1-6，从已经计算出的最短路径权重矩阵L计算出前驱矩阵Pi，在O(n*n*n)时间内。
+	 * 思路是判断某个位置的值是否是另一位置和权重矩阵的和，是的话，说明是计算来的，该位置的父节点是另一位置
+	 */
+	public void generatePi(double[][] L, double[][] W, int[][] Pi) {
+		int n=L.length;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				Pi[i][j]=MatrixGraph.NIL; // 初始化值
+				if(i==j){ // 自己跟自己无所谓路径一说
+					continue;
+				}
+				// 遍历该行所有，看看是从哪里过来的，直接过来的情况也包含在内了，
+				// 比如0到4权重是-4且它是最短路径，那么i=0，j=4，k=0时更新的4的父结点是0
+				for (int k = 0; k < n; k++) {
+					if(L[i][j] == L[i][k] + W[k][j] && k!=j){ // W对角线上都是0，成立但无意义
+						Pi[i][j]=k;
+					}
+				}
+			}
+		}
+	}
+	// @formatter:off
+	/**
+	 * Floyd-Warshall算法，自底向上递增次序计算最短路径权重Dn的值。
+	 * 算法时间复杂度O(n*n*n)
+	 * 不太好理解，书里给出伪代码时没结合上边的说明。
+	 * 注意k是可变的，解释里k是增长的，也就是伪代码中，k先从1开始，那么子集里只有结点1，
+	 * 所以D1中的最短路径，要么是原本就连到的，要么是通过结点1连到的。当k为2时，子集里有结点1和2，D2中的最短路径，
+	 * 是原本连到的，或是通过结点1或2或者都经过。这样一直到k为n，解释里第一个点说的是这个意思。
+	 * 第二个点说的意思是，在k增加时，最短路径可能从原来的最短路径的一个分叉中连到结点k，但仍包含在结点1、2、...k中，
+	 */
+	public double[][] FloydWarshall(double[][] W) {
+	// @formatter:on
+		int n=W.length;
+		double[][] Dk_1=W;
+		for (int k = 0; k < n; k++) {
+			double[][] Dk=new double[n][n];
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					Dk[i][j]=Double.min(Dk_1[i][j], Dk_1[i][k] + Dk_1[k][j]);
+				}
+			}
+			Dk_1=Dk;
+		}
+		return Dk_1;
+	}
 }
