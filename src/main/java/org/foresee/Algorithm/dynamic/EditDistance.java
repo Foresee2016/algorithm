d11a3f7d3cd53202adef00c6ec97e800ae1dad46
Foresee <1123520670@qq.com>
2019-01-01 20:51:13 +0800

    Chapter 15 think question 5, give up temporarily.

--- src/main/java/org/foresee/Algorithm/dynamic/EditDistance.java
+++ src/main/java/org/foresee/Algorithm/dynamic/EditDistance.java
@@ -1,9 +1,12 @@
 package org.foresee.Algorithm.dynamic;
 
 /**
- * 算法导论，第15章动态规划，课后练习15-5，编辑距离问题。 
- * 感觉这个问题不全，应该有个每种操作的cost值，不然全用insert就行了。
+ * 算法导论，第15章动态规划，课后练习15-5，编辑距离问题。
+ * 这里自己定义代价值，书里没写，但复制和替换要小于删除和插入，不然一直插入肯定最快。
+ * 这个题有些复杂，还是先放着吧。光是把这些演算一遍就好一会儿。
  */
 public class EditDistance {
+    public static void main(String[] args) {
 
+    }
 }
